cda1d0d34847806ba6df987839904ecd3486c16b
Roman Beskokotov <roma@gmail.com>
2025-09-22 18:58:31 +0300

    Определил структуру проекта, созал движок для алхемии и вывод данныз из env файла

---
+++ src/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    DB_PORT: int
+
+    @property
+    def DATABASE_URL_sync(self):
+        return f'postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+settings = Settings()
---
+++ src/database.py
@@ -0,0 +1,12 @@
+
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
+from sqlalchemy import MetaData, create_engine
+from config import settings
+
+engine = create_engine(
+    url=settings.DATABASE_URL_sync,
+    echo=True
+)
+
+class Base(DeclarativeBase):
+    metadata = MetaData()
