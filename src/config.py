029b55480b09c3ed23f7bd266059980e3232a447
Roman Beskokotov <roma@gmail.com>
2025-09-23 17:45:29 +0300

    Протестировал систему в контейнерах, все ок

--- src/config.py
+++ src/config.py
@@ -5,13 +5,13 @@ class Settings(BaseSettings):
     DB_HOST: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
-    DB_NAME: str
+    POSTGRES_DB: str
     DB_PORT: int
     DB_MODE: str
 
     @property
     def DATABASE_URL_sync(self):
-        return f'postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        return f'postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}'
 
     model_config = SettingsConfigDict(env_file='dev.env')
 
